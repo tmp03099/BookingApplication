988393bca66e2da3efc65370de5d0b555d2bad8d
My Phung Tieu <tmp03099@gmail.com>
2022-09-24 22:06:49 -0400

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { BookingPageComponent } from './booking-page/booking-page.component';       
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    BookingPageComponent
   ],
   imports: [
     BrowserModule,
